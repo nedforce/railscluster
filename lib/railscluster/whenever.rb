dd5d4e4560d8bd1da21d56be14cdb906b7d165c0
Arthur Holstvoogd <a.holstvoogd@nedforce.nl>
2013-05-24 14:12:54 +0200

    Whenever insits on using the release path, so always update..

--- lib/railscluster/whenever.rb
+++ lib/railscluster/whenever.rb
@@ -1,13 +1,5 @@
 Capistrano::Configuration.instance(:must_exist).load do
-  require "whenever/capistrano/recipes"
-
-  # Write the new cron jobs near the end.
-  before "deploy:finalize_update" do
-    whenever.update_crontab if changed? 'config/schedule.rb'
-  end
-
-  # If anything goes wrong, undo.
-  after "deploy:rollback", "whenever:update_crontab"
+  require "whenever/capistrano"
 
   set :whenever_command,      "#{bundle_cmd} exec whenever"
   set :whenever_environment,  defer { rails_env }
