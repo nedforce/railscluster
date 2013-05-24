e53adef2026de7fbb407dd5eafdee80538ad966e
Arthur Holstvoogd <a.holstvoogd@nedforce.nl>
2013-05-24 14:31:10 +0200

    Add Thin oob GC support: remove default GC profiling

--- lib/railscluster/thin_oob_gc.rb
+++ lib/railscluster/thin_oob_gc.rb
@@ -1,9 +1,8 @@
-GC::Profiler.enable
 class Thin::Backends::Base
   def connection_finished_with_oobgc(connection)
     connection_finished_without_oobgc(connection)
     if empty?
-      GC::Profiler.clear
+      GC::Profiler.clear if GC::Profiler.enabled?
       GC.start
     end
   end
