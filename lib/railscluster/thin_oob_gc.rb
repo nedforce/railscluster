33dda36530dac038716c84df605fbfa35408d8aa
Arthur Holstvoogd <a.holstvoogd@nedforce.nl>
2013-06-04 12:07:48 +0200

    Check for thin before reopenign the class

--- lib/railscluster/thin_oob_gc.rb
+++ lib/railscluster/thin_oob_gc.rb
@@ -1,10 +1,12 @@
-class Thin::Backends::Base
-  def connection_finished_with_oobgc(connection)
-    connection_finished_without_oobgc(connection)
-    if empty?
-      GC::Profiler.clear if GC::Profiler.enabled?
-      GC.start
+if defined?(Thin)
+  class Thin::Backends::Base
+    def connection_finished_with_oobgc(connection)
+      connection_finished_without_oobgc(connection)
+      if empty?
+        GC::Profiler.clear if GC::Profiler.enabled?
+        GC.start
+      end
     end
+    alias_method_chain :connection_finished, :oobgc
   end
-  alias_method_chain :connection_finished, :oobgc
 end
