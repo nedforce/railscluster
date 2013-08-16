b49e701f334b11e798752965eba730a119166e34
Arthur Holstvoogd <a.holstvoogd@nedforce.nl>
2013-08-16 12:48:29 +0200

    Add file..

---
+++ lib/railscluster/capistrano/sidekiq.rb
@@ -0,0 +1,21 @@
+Capistrano::Configuration.instance(:must_exist).load do
+
+  after 'deploy:restart', 'sidekiq.restart'
+
+  namespace :sidekiq do
+    desc "Start the sidekiq daemon" 
+    task :start, :roles => :app do
+      run "cluster_service sidekiq start" 
+    end
+
+    desc "Restart the sidekiq daemon" 
+    task :restart, :roles => :app do
+      run "cluster_service sidekiq restart" 
+    end
+
+    desc "Stop the sidekiq daemon" 
+    task :stop, :roles => :app do
+      run "cluster_service sidekiq stop" 
+    end
+  end
+end
